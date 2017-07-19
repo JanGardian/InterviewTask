e308c5d3ba58737ceb511f63a61b8fb0787a68b8
root <root@debian>
2017-07-19 18:10:09 +0200

    Final commit

--- html/aggregation.php
+++ html/aggregation.php
@@ -2,33 +2,37 @@
 
 // http://wiki.hashphp.org/PDO_Tutorial_for_MySQL_Developers
 
+// setting of PDO, db and credentials
 $db = new PDO('mysql:host=localhost;dbname=clock_db', 'root', 'fouvet3SAN');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 
+// getting current day 
 $today = date("Y-m-d");
 $dailyStopSum = 0;
 
+// Select from logging table to summary of all stop durations
 try {
-	$stmt = $db->prepare("SELECT * FROM logging WHERE DATE(created) = CURDATE()");
+        $stmt = $db->prepare("SELECT * FROM logging WHERE DATE(created) = CURDATE()");
         $stmt->execute();
         if($stmt !== false) {
                 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 foreach($rows as $row) {
-			$dailyStopSum = $dailyStopSum + ((int) $row['duration']);
+                        $dailyStopSum = $dailyStopSum + ((int) $row['duration']);
                 };
         }
 } catch ( Exception $e ) {
     print_r($e->getMessage());
 }
 
+// Insert into daily_aggregations current day and sum of all stop duration for current day 
 try {
-	$stmtInsert = $db->prepare("INSERT INTO daily_aggregations (date, stop_sum) VALUES (:date, :sum)");
-    	$stmtInsert->execute(array(':date' => $today, ':sum' => $dailyStopSum));
+        $stmtInsert = $db->prepare("INSERT INTO daily_aggregations (date, stop_sum) VALUES (:date, :sum)");
+        $stmtInsert->execute(array(':date' => $today, ':sum' => $dailyStopSum));
 
 } catch ( Exception $e ) {
         print_r($e->getMessage());
 }
 
-
 ?>
+
--- html/server.php
+++ html/server.php
@@ -1,70 +1,72 @@
 <?php
 
 // http://wiki.hashphp.org/PDO_Tutorial_for_MySQL_Developers
-
+// setting of PDO, db and credentials
 $db = new PDO('mysql:host=localhost;dbname=clock_db', 'root', 'fouvet3SAN');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 
-
+// loading data from POST sent by ajax
 $method = $_POST['type'];
 $unixTime = $_POST['time'];
+
 $stopFinished;
 $lastId;
 $stopTime;
 
+// Select from db to get if Stop was already clicked or not
 try {
-	$stmt = $db->prepare("SELECT * FROM logging WHERE id = (SELECT MAX(id) FROM logging)");
-	$stmt->execute();
-	if ($stmt->rowCount() === '0') {
-		$stopFinished =1;	
-	}
-	if($stmt !== false) {	
-		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		foreach($rows as $row) {
-			$stopFinished = $row['finished'];
-			$lastId = $row['id'];
-			$stopTime = $row['stop_time'];
-		};
-	}
+        $stmt = $db->prepare("SELECT * FROM logging WHERE id = (SELECT MAX(id) FROM logging)");
+        $stmt->execute();
+        if ($stmt->rowCount() === '0') {
+                $stopFinished =1;
+        }
+        if($stmt !== false) {
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                foreach($rows as $row) {
+                        $stopFinished = $row['finished'];
+                        $lastId = $row['id'];
+                        $stopTime = $row['stop_time'];
+                };
+        }
 
 } catch ( Exception $e ) {
     print_r($e->getMessage());
 }
-var_dump((int)$unixTime);
-var_dump($unixTime);
-var_dump((int)$stopTime);
-var_dump($stopTime);
 
-//print_r($stopFinished);
-//print_r($lastId);
-
+// When ajax sent method stopTime new row/log will be inserted to logging table
+// If previous click was also stopTime it will be ignored and no new log inserted
 if ($method === 'stopTime') {
-	if ($stopFinished == '1') {
-		try {
-			$stmtStop = $db->prepare("INSERT INTO logging (stop_time) VALUES (:utime)");
-			$stmtStop->execute(array(':utime' => (int) $unixTime/1000));
+        if ($stopFinished == '1') {
+                try {
+                        $stmtStop = $db->prepare("INSERT INTO logging (stop_time) VALUES (:utime)");
+                        // javascript unix time is in mili seconds and mysql in seconds so had to convert
+						$stmtStop->execute(array(':utime' => (int) $unixTime/1000));
+						
+                } catch ( Exception $e ) {
 
-			
-		} catch ( Exception $e ) {
-			
-			print_r($e->getMessage());
-		}
-		
-	}
+                        print_r($e->getMessage());
+                }
+
+        }
 }
 
+// When ajax sent methor restartTime row will be update only if previous click was stopTime
 if ($method === 'restartTime') {
-	if ($stopFinished == '0') {
-		$duration = (((int) $unixTime/1000) - $stopTime);
-		var_dump($duration);
-		try {
-			$stmtStart = $db->prepare("UPDATE logging SET finished=?, restart_time=?, duration=? WHERE id=?");
-			$stmtStart->execute(array(1,(int) $unixTime/1000, $duration, $lastId));
-			$affected_rows = $stmtStart->rowCount();
-		} catch ( Exception  $e ) {
+        if ($stopFinished == '0') {
+				// duration of stopped time is counted as difference between unixTime sent by ajax when clicked restartTime
+				// and previous unix time when was clicked stopTime
+                $duration = (((int) $unixTime/1000) - $stopTime);
+                var_dump($duration);
+                try {
+                        $stmtStart = $db->prepare("UPDATE logging SET finished=?, restart_time=?, duration=? WHERE id=?");
+                        $stmtStart->execute(array(1,(int) $unixTime/1000, $duration, $lastId));
+                        $affected_rows = $stmtStart->rowCount();
+						
+                } catch ( Exception  $e ) {
 
-			print_r($e->getMessage());
-		}
-	}
+                        print_r($e->getMessage());
+                }
+        }
 }
+
